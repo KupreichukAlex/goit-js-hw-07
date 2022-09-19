501d16cebd8fb3dc835099ad651e650aa1026d71
KupreichukAlex <98807339+KupreichukAlex@users.noreply.github.com>
2022-09-19 23:19:59 +0300

    finished

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const makeGalleryItemMarkup = (arr) => {
+    return arr
+        .map(({ description: descr, preview, original: orig }) => {
+            return `<div class="gallery__item">
+                        <a class="gallery__link" href="large-image.jpg">
+                            <img
+                                class="gallery__image"
+                                src="${preview}"
+                                data-source="${orig}"
+                                alt="${descr}"
+                                />
+                        </a>
+                    </div>`;
+        })
+        .join('');
+};
+
+const galleryContainerRef = document.querySelector('.gallery');
+galleryContainerRef.innerHTML = makeGalleryItemMarkup(galleryItems);
+
+const createLightboxInstance = (e) => {
+    const targetedImgUrl = e.target.dataset.source;
+    const instance = basicLightbox.create(`
+    <img src="${targetedImgUrl}" width="800" height="600">
+    `);
+
+    instance.show(
+        document.addEventListener('keydown', (e) => {
+            if (e.key && e.code === 'Escape') {
+                instance.close();
+            }
+        })
+    );
+};
+
+const onGalleryImgClick = (e) => {
+    if (e.target.nodeName !== 'IMG') return;
+
+    createLightboxInstance(e);
+    e.preventDefault();
+};
+
+galleryContainerRef.addEventListener('click', onGalleryImgClick);
+
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = itemMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+function itemMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `<a class="gallery__item" href= "${original}">
+  <img class="gallery__image" src= "${preview}" alt="${description}" />
+</a>`
+        })
+    .join("")
+}
+
+galleryContainer.addEventListener('click', e => {
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+        return
+    }
+
+    
+    new SimpleLightBox('.gallery a', {
+        captionsData: `alt`,
+        captionDelay: 250,
+        animationSpeed: 250,
+        
+    });
+
+})
+// console.log(galleryItems);
+
+
